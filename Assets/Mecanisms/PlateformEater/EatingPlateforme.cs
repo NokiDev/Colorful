393e0175b937e3cf576d22d0e90348d869764dc8
Timothé <noki.dev@gmail.com>
2017-12-03 03:32:26 +0100

    Cleanup and re organizing a little file system

---
+++ Assets/Mecanisms/PlateformEater/EatingPlateforme.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatingPlateforme : MonoBehaviour {
+
+    private Collider _ParentCollider;
+
+	// Use this for initialization
+	void Start () {
+        _ParentCollider = gameObject.transform.parent.GetComponent<Collider>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
